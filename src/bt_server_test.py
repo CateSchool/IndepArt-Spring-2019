e8eb8dcaa29d58e42f3d0196481441fb5773f74d
gamma-delta <joshua.j.park@gmail.com>
2018-12-16 14:00:55 -0800

    Changed uuid

--- src/bt_server_test.py
+++ src/bt_server_test.py
@@ -1,16 +1,17 @@
 from bluetooth import *
 
-server_sock = BluetoothSocket()
-server_sock.bind(("", PORT_ANY))
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid= "8f6f844b-a197-48d8-a2c6-da956b01cf5f" #randomly yote from http://guid-convert.appspot.com/
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service(server_sock, "Gamma's Server",
-    service_id=uuid,
-    service_classes=[uuid, SERIAL_PORT_CLASS]
+advertise_service( server_sock, "SampleServer",
+    service_id = uuid,
+    service_classes = [ uuid, SERIAL_PORT_CLASS ],
+    profiles = [ SERIAL_PORT_PROFILE ], 
 #   protocols = [ OBEX_UUID ] 
     )
                    
