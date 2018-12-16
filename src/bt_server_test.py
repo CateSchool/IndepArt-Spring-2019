f97a329acf35bfc4453053a8d712fc63467ad14b
gamma-delta <joshua.j.park@gmail.com>
2018-12-15 23:05:49 -0800

    Update src/bt_server_test.py

--- src/bt_server_test.py
+++ src/bt_server_test.py
@@ -9,7 +9,7 @@ port = server_sock.getsockname()[1]
 uuid= "8f6f844b-a197-48d8-a2c6-da956b01cf5f" #randomly yote from http://guid-convert.appspot.com/
 
 advertise_service(server_sock, "Gamma's Server",
-    service_uuid=uuid,
+    service_id=uuid,
     service_classes=[uuid, SERIAL_PORT_CLASS]
 #   protocols = [ OBEX_UUID ] 
     )
